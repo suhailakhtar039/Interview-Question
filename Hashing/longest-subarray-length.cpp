bf100e2cb076368a155abacd19ecb8c4afb7950e
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-09-21 23:38:08 +0530

    longest subarray length

---
+++ Hashing/longest-subarray-length.cpp
@@ -0,0 +1,28 @@
+// Author : Suhail Akhtar
+// Date : 20-09-22
+#include<bits/stdc++.h>
+using namespace std;
+int solve(vector<int> &v){
+    int n = v.size();
+    unordered_map<int,int> m;
+    int sum=0,mxLen=0;
+    for(int i=0; i<n; i++){
+        sum+=v[i]==0?-1:1;
+        if(sum==1){
+            mxLen=i+1;
+        }else if(m.count(sum)==0){
+            m[sum]=i;
+        }
+
+        if(m.count(sum-1)){
+            mxLen=max(mxLen,i-m[sum-1]);
+        }
+    }
+    return mxLen;
+}
+int main(){
+    int n;cin>>n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++)cin>>v[i];
+    cout<<solve(v);
+}
