e48bfd8ed6a6dfce2b67b8f2f313e082005cd277
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-10-02 23:09:05 +0530

    creating a tree

---
+++ Tree Data Structure/creating-a-tree.cpp
@@ -0,0 +1,30 @@
+// Author : Suhail Akhtar
+// Date : 02-10-22
+#include<bits/stdc++.h>
+using namespace std;
+class TreeNode{
+    public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x):val(x),left(NULL),right(NULL){}
+};
+TreeNode *createTree(){
+    int x;cin>>x;
+    if(x==-1)return NULL;
+    TreeNode *root = new TreeNode(x);
+    cout<<"Enter left child of "<<x<<" (or -1 to stop):";
+    root->left = createTree();
+    cout<<"Enter right child of "<<x<<" (or -1 to stop):";
+    root->right = createTree();
+    return root;
+}
+void preorder(TreeNode* root){
+    if(root==NULL)return ;
+    cout<<root->val<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+int main(){
+    TreeNode* root = createTree();
+    preorder(root);
+}
