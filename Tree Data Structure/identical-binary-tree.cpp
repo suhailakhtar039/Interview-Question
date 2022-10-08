bda0aea6cf58ad64a29f757a46e2a17e9182dd5c
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-10-08 23:40:26 +0530

    identical binary tree

---
+++ Tree Data Structure/identical-binary-tree.cpp
@@ -0,0 +1,47 @@
+// Author : Suhail Akhtar
+// Date : 08-10-22
+#include<bits/stdc++.h>
+using namespace std;
+class TreeNode{
+    public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x):val(x),left(NULL),right(NULL){}
+};
+TreeNode *createTree(){
+    int x;cin>>x;
+    if(x==-1)return NULL;
+    TreeNode *root = new TreeNode(x);
+    cout<<"Enter left child of "<<x<<" (or -1 to stop):";
+    root->left = createTree();
+    cout<<"Enter right child of "<<x<<" (or -1 to stop):";
+    root->right = createTree();
+    return root;
+}
+TreeNode* createBST(TreeNode* root,int val){
+    if(root==NULL){
+        return new TreeNode(val);
+    }
+    if(val<root->val)
+        root->left = createBST(root->left,val);
+    else
+        root->right = createBST(root->right,val);
+    return root;
+}
+void preorder(TreeNode* root){
+    if(root==NULL)return ;
+    cout<<root->val<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+// ====================
+int solve(TreeNode* root1,TreeNode* root2){
+    if(!root1 && !root2)return true;
+    if(!root1 || !root2) return false;
+    return root1->val==root2->val && solve(root1->left,root2->left) && solve(root1->right,root2->right);
+}
+int main(){
+    TreeNode* root1 = createTree();
+    TreeNode* root2 = createTree();
+    cout<<solve(root1,root2);
+}
