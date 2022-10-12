740b7760687de8a84d03c095914349525f4645fe
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-10-12 23:32:20 +0530

    max-depth

---
+++ Tree Data Structure/max-depth-of-binary-tree.cpp
@@ -0,0 +1,45 @@
+// Author : Suhail Akhtar
+// Date : 12-10-22
+#include<bits/stdc++.h>
+using namespace std;
+class TreeNode{
+    public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x):val(x),left(NULL),right(NULL){}
+};
+TreeNode *createTree(){
+    int x;cin>>x;
+    if(x==-1)return NULL;
+    TreeNode *root = new TreeNode(x);
+    cout<<"Enter left child of "<<x<<" (or -1 to stop):";
+    root->left = createTree();
+    cout<<"Enter right child of "<<x<<" (or -1 to stop):";
+    root->right = createTree();
+    return root;
+}
+TreeNode* createBST(TreeNode* root,int val){
+    if(root==NULL){
+        return new TreeNode(val);
+    }
+    if(val<root->val)
+        root->left = createBST(root->left,val);
+    else
+        root->right = createBST(root->right,val);
+    return root;
+}
+void preorder(TreeNode* root){
+    if(root==NULL)return ;
+    cout<<root->val<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+// ========
+int solve(TreeNode* root){
+    if(!root)return 0;
+    return 1+max(solve(root->left),solve(root->right));
+}
+int main(){
+    TreeNode* root = createTree();
+    cout<<solve(root);
+}
