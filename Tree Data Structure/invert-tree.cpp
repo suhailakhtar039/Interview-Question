f210d03902a9b3b59feeb2486ab8c5ad69ac4e60
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-10-13 23:31:22 +0530

    invert binary tree

---
+++ Tree Data Structure/invert-tree.cpp
@@ -0,0 +1,52 @@
+// Author : Suhail Akhtar
+// Date : 13-10-22
+#include<bits/stdc++.h>
+using namespace std;
+class TreeNode{
+    public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x):val(x),left(NULL),right(NULL){}
+};
+TreeNode *createTree(){
+    int x;cin>>x;
+    if(x==-1)return NULL;
+    TreeNode *root = new TreeNode(x);
+    cout<<"Enter left child of "<<x<<" (or -1 to stop):";
+    root->left = createTree();
+    cout<<"Enter right child of "<<x<<" (or -1 to stop):";
+    root->right = createTree();
+    return root;
+}
+TreeNode* createBST(TreeNode* root,int val){
+    if(root==NULL){
+        return new TreeNode(val);
+    }
+    if(val<root->val)
+        root->left = createBST(root->left,val);
+    else
+        root->right = createBST(root->right,val);
+    return root;
+}
+void preorder(TreeNode* root){
+    if(root==NULL)return ;
+    cout<<root->val<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+// ========
+void solveUtil(TreeNode *root){
+    if(!root)return;
+    solveUtil(root->left);
+    solveUtil(root->right);
+    swap(root->left,root->right);
+}
+TreeNode* solve(TreeNode* root){
+    solveUtil(root);
+    return root;
+}
+int main(){
+    TreeNode *root = createTree();
+    TreeNode *inverted = solve(root);
+    preorder(inverted);
+}
