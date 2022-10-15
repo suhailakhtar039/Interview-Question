668841f1f38e68596c6d3b3bb8b0806d875d3ab5
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-10-16 00:41:26 +0530

    bottom down approach distinct subsequence

--- Dynamic Programming/distinct-subsequences.cpp
+++ Dynamic Programming/distinct-subsequences.cpp
@@ -2,6 +2,7 @@
 // Date : 16-10-22
 #include<bits/stdc++.h>
 using namespace std;
+// top down approach
 int mem[701][701];
 int distinct(string a,string b,int n,int m){
     if(m<0)return 1;
@@ -14,9 +15,19 @@ int distinct(string a,string b,int n,int m){
 }
 int solve(string a,string b){
     int n = a.length(),m = b.length();
-    for(int i=0; i<=700; i++)
-    for(int j=0; j<=700; j++)mem[i][j]=-1;
-    return distinct(a,b,n-1,m-1);
+    int dis[n+1][m+1]={0};
+    for(int i=0; i<=n; i++)dis[i][0]=1;
+    for(int j=0; j<=m; j++)dis[0][j]=0;
+    dis[0][0]=1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(a[i-1]==b[j-1])
+                dis[i][j]=dis[i-1][j]+dis[i-1][j-1];
+            else
+                dis[i][j]=dis[i-1][j];
+        }
+    }
+    return dis[n][m];
 }
 int main(){
     string a,b;cin>>a>>b;
